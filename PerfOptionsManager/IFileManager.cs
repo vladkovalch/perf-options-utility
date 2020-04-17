fbc5fd46988d78ebef85010bf824fdef0b00aa41
vladkoval4yk399@gmail.com <vladkoval4yk399@gmail.com>
2020-04-17 15:03:28 +0300

    Initial commit

---
+++ PerfOptionsManager/IFileManager.cs
@@ -0,0 +1,10 @@
+﻿namespace PerfOptionsManager
+{
+	public interface IFileManager
+	{
+		bool CreateFile(string filePath);
+		bool CreateFile(string filePath, string text);
+		string ReadFile(string filePath);
+		bool WriteFile(string filePath, string text);
+	}
+}
